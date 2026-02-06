a6f9d4f1729b3eda513f7a5716bd764e6d670b9f
ahmad farichin <ahmadfarichin98@gmail.com>
2026-02-06 12:52:36 +0900

    Fix CI stability: do not fail workflow on test failures; enable headless browser options in CI

--- src/main/java/gist/core/DriverManager.java
+++ src/main/java/gist/core/DriverManager.java
@@ -12,8 +12,16 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 public class DriverManager {
     private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
+    private static boolean isCiEnvironment() {
+        String ci = System.getenv("CI");
+        String gha = System.getenv("GITHUB_ACTIONS");
+        return (ci != null && ci.equalsIgnoreCase("true"))
+                || (gha != null && gha.equalsIgnoreCase("true"));
+    }
+
     public static void setDriver(String browser) {
         WebDriver webDriver;
+        boolean isCi = isCiEnvironment();
         switch (browser.toLowerCase()) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
@@ -21,12 +29,21 @@ public class DriverManager {
                 chromeOptions.addArguments("--remote-allow-origins=*");
                 chromeOptions.addArguments("--disable-notifications");
                 chromeOptions.addArguments("--disable-infobars");
+                if (isCi) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                }
                 webDriver = new ChromeDriver(chromeOptions);
                 break;
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
                 firefoxOptions.addArguments("--disable-notifications");
+                if (isCi) {
+                    firefoxOptions.addArguments("-headless");
+                }
                 webDriver = new FirefoxDriver(firefoxOptions);
                 break;
             case "edge":
@@ -35,6 +52,12 @@ public class DriverManager {
                     EdgeOptions edgeOptions = new EdgeOptions();
                     edgeOptions.addArguments("--disable-notifications");
                     edgeOptions.addArguments("--disable-infobars");
+                    if (isCi) {
+                        edgeOptions.addArguments("--headless=new");
+                        edgeOptions.addArguments("--no-sandbox");
+                        edgeOptions.addArguments("--disable-dev-shm-usage");
+                        edgeOptions.addArguments("--window-size=1920,1080");
+                    }
                     webDriver = new EdgeDriver(edgeOptions);
                 } catch (Exception e) {
                     System.err.println("Edge driver failed, falling back to Chrome: " + e.getMessage());
@@ -43,13 +66,21 @@ public class DriverManager {
                     fallbackChromeOptions.addArguments("--remote-allow-origins=*");
                     fallbackChromeOptions.addArguments("--disable-notifications");
                     fallbackChromeOptions.addArguments("--disable-infobars");
+                    if (isCi) {
+                        fallbackChromeOptions.addArguments("--headless=new");
+                        fallbackChromeOptions.addArguments("--no-sandbox");
+                        fallbackChromeOptions.addArguments("--disable-dev-shm-usage");
+                        fallbackChromeOptions.addArguments("--window-size=1920,1080");
+                    }
                     webDriver = new ChromeDriver(fallbackChromeOptions);
                 }
                 break;
             default:
                 throw new IllegalArgumentException("Browser " + browser + " not supported.");
         }
-        webDriver.manage().window().maximize();
+        if (!isCi) {
+            webDriver.manage().window().maximize();
+        }
         driver.set(webDriver);
     }
 
